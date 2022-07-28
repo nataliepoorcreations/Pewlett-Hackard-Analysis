860f5d26d0ec1c6de07315387fc456303a9b2aac
Matthew Poor <poorfamily@Matthews-MacBook-Air-2.local>
2022-07-27 21:09:12 -0400

    adding deliverable 1 and 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,30 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.bith_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+-- Remove duplicate entries
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire.
+SELECT count(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count(title) DESC;
